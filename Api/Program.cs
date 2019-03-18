fdd29e486b4d861f1610edaff784fdb3d8b0eb2e
Chris Golden <crgolden@msn.com>
2019-03-18 07:55:21 -0500

    Adding Serilog

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,21 +1,54 @@
 ﻿namespace Clarity.Api
 {
+    using System;
     using System.Threading;
     using System.Threading.Tasks;
     using Core;
+    using Microsoft.ApplicationInsights.Extensibility;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using Serilog;
+    using Serilog.Events;
 
     public class Program
     {
         // https://docs.microsoft.com/en-us/aspnet/core/migration/1x-to-2x/#update-main-method-in-programcs
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            using (var tokenSource = new CancellationTokenSource())
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.ApplicationInsights(
+                    telemetryConfiguration: TelemetryConfiguration.Active,
+                    telemetryConverter: TelemetryConverter.Traces)
+                .WriteTo.File(
+                    path: @"D:\home\LogFiles\Application\Clarity.Api.txt",
+                    fileSizeLimitBytes: 1_000_000,
+                    shared: true,
+                    flushToDiskInterval: TimeSpan.FromSeconds(1),
+                    rollOnFileSizeLimit: true)
+                .CreateLogger();
+            try
             {
-                var webHost = BuildWebHost(args);
-                await webHost.MigrateDatabaseAsync(tokenSource.Token);
-                await webHost.RunAsync(tokenSource.Token);
+                Log.Information("Starting web host");
+                using (var tokenSource = new CancellationTokenSource())
+                {
+                    var webHost = BuildWebHost(args);
+                    await webHost.MigrateDatabaseAsync(tokenSource.Token);
+                    await webHost.RunAsync(tokenSource.Token);
+                    return 0;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
             }
         }
 
@@ -23,9 +56,8 @@
             WebHost
                 .CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(configBuilder => configBuilder.AddAzureKeyVault())
-                .ConfigureLogging(loggingBuilder => loggingBuilder.AddLogging())
-                .UseApplicationInsights()
                 .UseStartup<Startup>()
+                .UseSerilog()
                 .Build();
     }
 }
