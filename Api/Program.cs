77d8f6d0d169242cbac7fe17f728e7186a667a13
Chris Golden <crgolden@msn.com>
2019-03-24 15:28:33 -0500

    Elasticsearch test 13

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,16 +1,10 @@
 ﻿namespace Clarity.Api
 {
-    using System;
     using System.Threading;
     using System.Threading.Tasks;
     using Core;
-    using Microsoft.ApplicationInsights.Extensibility;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
-    using Serilog;
-    using Serilog.Events;
-    using Serilog.Formatting.Elasticsearch;
-    using Serilog.Sinks.Elasticsearch;
 
     public class Program
     {
@@ -30,30 +24,7 @@
                 .CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(configBuilder => configBuilder.AddAzureKeyVault())
                 .UseStartup<Startup>()
-                .UseSerilog((context, loggerConfiguration) => loggerConfiguration
-                    .MinimumLevel.Debug()
-                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                    .Enrich.FromLogContext()
-                    .WriteTo.Console()
-                    .WriteTo.ApplicationInsights(
-                        telemetryConfiguration: TelemetryConfiguration.Active,
-                        telemetryConverter: TelemetryConverter.Traces)
-                    .WriteTo.File(
-                        path: "D:\\home\\LogFiles\\Application\\Clarity.Api.txt",
-                        fileSizeLimitBytes: 1_000_000,
-                        shared: true,
-                        flushToDiskInterval: TimeSpan.FromSeconds(1),
-                        rollOnFileSizeLimit: true)
-                    .WriteTo.Elasticsearch(
-                        options: new ElasticsearchSinkOptions(node: new Uri(
-                            uriString: "https://af7e26e2-9e3e-4841-8561-1fd0048a1bff.cloudapp.net:9200")
-                        )
-                        {
-                            AutoRegisterTemplate = true,
-                            AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
-                            IndexFormat = "Clarity.Api-logs-index",
-                            CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true)
-                        }))
+                .UseSerilog("Clarity.Api")
                 .Build();
     }
 }
