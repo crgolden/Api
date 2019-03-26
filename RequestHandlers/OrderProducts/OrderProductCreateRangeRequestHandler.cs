902421651474373e0c77ce9f42a4a49618efffff
Chris Golden <crgolden@msn.com>
2019-03-26 16:53:57 -0500

    Adding Order and Product to OrderProduct create range

--- RequestHandlers/OrderProducts/OrderProductCreateRangeRequestHandler.cs
+++ RequestHandlers/OrderProducts/OrderProductCreateRangeRequestHandler.cs
@@ -1,5 +1,7 @@
 ﻿namespace Clarity.Api.OrderProducts
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using AutoMapper;
     using Abstractions;
     using Microsoft.EntityFrameworkCore;
@@ -8,6 +10,25 @@
     {
         public OrderProductCreateRangeRequestHandler(DbContext context, IMapper mapper) : base(context, mapper)
         {
+        }
+
+        public override async Task<(OrderProductModel[], object[][])> Handle(OrderProductCreateRangeRequest request, CancellationToken token)
+        {
+            var models = new OrderProductModel[request.Models.Length];
+            var keyValues = new object[request.Models.Length][];
+            for (var i = 0; i < request.Models.Length; i++)
+            {
+                var entity = Mapper.Map<OrderProduct>(request.Models[i]);
+                var entityEntry = Context.Entry(entity);
+                await entityEntry.Reference(x => x.Product).LoadAsync(token).ConfigureAwait(false);
+                await entityEntry.Reference(x => x.Order).LoadAsync(token).ConfigureAwait(false);
+                entityEntry.State = EntityState.Added;
+                models[i] = Mapper.Map<OrderProductModel>(entity);
+                keyValues[i] = new object[]{ request.Models[i].OrderId, request.Models[i].ProductId };
+            }
+
+            await Context.SaveChangesAsync(token).ConfigureAwait(false);
+            return (models, keyValues);
         }
     }
 }
